562c2ab3799881bf135c992f3a4b2e88909964fb
pratapm333 <pratap3090@gmail.com>
2024-10-22 18:22:05 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -1,18 +1,33 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req,res)=>{
-    res.send("Hi Hello")
-});
  
-app.use("/test", (req,res)=>{
-    res.send("Hello from the server")
-});
+app.get("/user", (req,res)=>{
+    res.send({"first_name": "Pratap", "last_name": "MP"})
+})
+
+app.post("/user", async (req,res)=>{
+   console.log(req.body)  
+   //saving data in DB
+    res.send("data is created in DB");
+})
+
+app.patch("/user", (req,res)=>{
+    res.send("Updated successfully")
+})
+app.delete("/user", (req,res)=>{
+    res.send("User deleted");
+})
 
 app.use("/hello", (req,res)=>{
     res.send("Hello hello hello")
 });
 
+
 app.listen(3000, () =>{
     console.log("Server is successfully listen")
+});
+
+app.use("/", (req,res)=>{
+    res.send("Hi Hello")
 });
