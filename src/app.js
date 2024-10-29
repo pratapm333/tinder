a2c8c57fbd3efa107cd597c73c4a909453203cc3
pratapm333 <pratap3090@gmail.com>
2024-10-29 15:00:25 +0530

    Login API

--- src/app.js
+++ src/app.js
@@ -84,6 +84,27 @@ app.post("/signup", async(req, res)=>{
    
 })
  
+app.post("/login", async(req, res) =>{
+    try{
+        const {emailId, password} = req.body;
+        const user = await User.findOne({emailId: emailId});
+
+        if(!user){
+            throw new Error("Invalid credentials");
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(isPasswordValid){
+            res.send("Login Sucessful!!!");
+        }
+        else{
+            throw new Error("Invalid credentials");
+        }
+        
+    }catch(err){
+            res.status(400).send("Error :" +err.message);
+    }
+})
 connectDB()
 .then(() =>{
     console.log("DB Connection is established")
